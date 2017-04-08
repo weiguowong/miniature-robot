53936ebc730ce847cb2c5acca0388f317c74e404
weiguowong <weiguowong@hotmail.com>
2017-04-08 14:34:56 +0800

    Create hello_world.c
    just a test

---
+++ hello_world.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+//this is a comment.
+/*
+this
+is
+a comment
+too.
+*/
+
+
+int main(){
+printf("hello world!\n");
+printf("hello world!\n");
+printf("The number printed %f %d!\n",32.2,15);//multi place holder
+printf("%d\n",18); //integer
+printf("%ld\n",18181818);//large integer
+printf("%f\n",18.888);//float
+printf("%lf\n",181818.8888);//large float
+printf("%c\n",'a');//char
+printf("%s\n","hello world");//string
+printf("%x\n",16);//hex
+return 0;
+}
